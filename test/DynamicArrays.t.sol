b96374d8701733c1bc5d91990bfddcd3890e2324
David Kulman <kulman.david@gmail.com>
2022-08-05 01:46:29 +0200

    Add Tests

---
+++ test/DynamicArrays.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract SimpleStoreTest is Test {
+    /// @dev Address of the SimpleStore contract.
+    DynamicArray public arr;
+
+    /// @dev Setup the testing environment.
+
+    function setUp() public {
+        arr = DynamicArray(HuffDeployer.deploy("DynamicArray"));
+    }
+
+    function testLength() public {
+        uint x = arr.length();
+        assertEq(x, 0x00);
+    }
+
+    function testPush() public {
+        bytes4 selector = bytes4(keccak256("push()"));
+
+        bytes memory data = abi.encodeWithSelector(selector, address(this));
+
+        (bool success, bytes memory returnData) = address(arr).call(data);
+
+        require(success, "ITS FUCKED");
+
+        uint length = arr.length();
+        assertEq(length, 1);
+    }
+}
+
+interface DynamicArray {
+    function push() external;
+
+    function popLast() external;
+
+    function show(uint256) external view returns (bytes32);
+
+    function length() external view returns (uint256);
+}
